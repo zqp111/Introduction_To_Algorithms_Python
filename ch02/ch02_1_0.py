f08e78d9308ab25e962a903f6b0882c921b229bc
zqp <zhangbo91100@163.com>
2019-12-15 15:28:03 +0800

    chapt 2 结构优化

--- ch02/ch02_1_0.py
+++ ch02/ch02_1_0.py
@@ -3,23 +3,23 @@ date: 2019/12/10
 author: zqp111
 正文和课后习题2-1-2 插入排序
 '''
-
+import random
 
 def InsertSort(inputArray, order='UP'):
-    temp = inputArray.copy()    # 防止改变原数列
-    for i, num in enumerate(temp[1:], start=1):
+    for i, num in enumerate(inputArray[1:], start=1):
         j = i-1
-        while j >=0 and (temp[j] > num if order.upper() == 'UP' 
-                         else (temp[j] < num if order.upper() == 'DOWN' 
+        while j >=0 and (inputArray[j] > num if order.upper() == 'UP' 
+                         else (inputArray[j] < num if order.upper() == 'DOWN' 
                          else False)):
-            temp[j+1] = temp[j]
+            inputArray[j+1] = inputArray[j]
             j -= 1
-        temp[j+1] = num
+        inputArray[j+1] = num
     
-    return temp
+    return inputArray
 
 if __name__ == "__main__":
-    A = [1, 2, 4, 3]
-    B = InsertSort(A,'up')
-    print(A)
-    print(B)
+    a = []
+    for _ in range(10):
+        a.append(random.randint(0, 99))
+    InsertSort(a,'up')
+    print(a)
